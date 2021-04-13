5b9ded96c9a541c86ef44e0e366f0e24e4acf7cd
RafaPalau <rafaeldonizetip@gmail.com>
2021-04-13 14:48:42 -0300

    theme colours defined

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,17 @@
+export default {
+  title: "dark",
+
+  colors: {
+    primary: "#01061B",
+    secondary: "#051738",
+    tertiary: "#1E3B8D",
+
+    white: "#ffffff",
+    black: "#000000",
+    gray: "#BFBFBF",
+
+    sucess: "#5080EA",
+    info: "#F98216",
+    warning: "#E44C4E",
+  },
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,17 @@
+export default {
+  title: "light",
+
+  colors: {
+    primary: "#DCDCDC",
+    secondary: "#FFFFFF",
+    tertiary: "#F5F5F5",
+
+    white: "#000000",
+    black: "#FFFFFF",
+    gray: "#BFBFBF",
+
+    sucess: "#22C55E",
+    info: "#4B6EC3",
+    warning: "#E13C6C",
+  },
+};
