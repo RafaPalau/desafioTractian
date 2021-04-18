1520c0eecf496374962628b84814d0208b58b11d
RafaPalau <rafaeldonizetip@gmail.com>
2021-04-17 22:03:37 -0300

    config server.js

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const { resolve } = require('path')
 
 const app = express()
 
-app.use('/', express.static(
-  resolve(
-    __dirname, './build'
-  )
-))
+app.use('/',
+  express.static(
+    resolve(
+      __dirname,
+      './build'
+    )
+  ))
 
 app.listen(process.env.PORT || 3000, (err) => {
   if (err) { return console.log(err) }
